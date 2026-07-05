b30b2d268604e65de605d4972b93bda0ea9efee2
Xolisa Nunge <xolisa@flume.co.za>
2026-07-06 00:08:24 +0200

    mini-challenge:  DOM change challenge.

---
+++ week-one/understanding-dom-and-browser/miniScript.js
@@ -0,0 +1,13 @@
+function changeHeading() {
+    const heading = document.getElementById("myHeading")
+    heading.textContent = "Welcome to the beginning of greatness";
+}
+
+function changeParagraph() {
+    document.getElementById("myParagraph").style.color = "#DE3163";
+}
+
+function changeBackgroundColor() {
+    document.getElementById("domSection").style.backgroundColor = "#F0FFFF"
+}
+
